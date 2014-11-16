bac1361b8c2dde0b3ddb7b76616b25029c1ca877
CottonValley <cottonvalley.app@gmail.com>
2014-11-16 18:09:10 +0900

    Walking Santa

--- Assets/Script/Object/Killer.cs
+++ Assets/Script/Object/Killer.cs
@@ -8,13 +8,19 @@ public class Killer : MonoBehaviour {
 	public int appear_lv = 0;
 	public string killer_name = "";
 	public string discription = "";
+	public float movePerFrame = 0.1f;
 
 	public List<GameObject> characters;
 	private GameObject target = null;
 	private bool moving = false;
 	private Vector2 toPos;
-	private float moveTime = 1f;
 	private float movingTimer = 0f;
+
+	private float walkTime = 8f;
+	private float walkTimer =0f;
+	private bool walking = false;
+	private float walkingCounter = 0f;
+	private Vector2 walkPos = Vector2.zero;
 
 	// Use this for initialization
 	void Start () {
@@ -25,21 +31,35 @@ public class Killer : MonoBehaviour {
 	void Update () {
 		if(!moving && characters.Count > 0){
 			moving = true;
+			walking = false;
+			walkTimer = 0;
+
 			target = characters.First();
 			var tempP = target.transform.position;
 			toPos =  new Vector2( transform.position.x-tempP.x, transform.position.y-tempP.y );
 			characters.Remove(target);
+			return;
+		}
+		else if( !(moving || walking) ){
+			walkTimer += Time.deltaTime;
+			if(walkTimer >= walkTime){
+				walking = true;
+				walkTimer = 0f;
+				SetWalk();
+			}
 		}
 	}
 
 	void FixedUpdate() {
 		if(target != null){
-			movingTimer += Time.deltaTime;
+			movingTimer += movePerFrame;
 			var nowPos = new Vector2(transform.position.x, transform.position.y);
-			rigidbody2D.MovePosition( nowPos - toPos*Time.deltaTime);
+			rigidbody2D.MovePosition( nowPos - toPos*movePerFrame);
+			var distance = nowPos-toPos*Time.deltaTime;
+			Debug.Log (" x : "+distance.x+"   y : "+distance.y);
 
-			var percent = movingTimer/moveTime;
-			if(percent >= 1){
+			if(movingTimer >= 1){
+				Debug.Log (" x : "+distance.x+"   y : "+distance.y+"     toX : "+toPos.x+"   toY : "+toPos.y);
 				movingTimer = 0;
 				moving = false;
 
@@ -52,5 +72,25 @@ public class Killer : MonoBehaviour {
 				target = null;
 			}
 		}
+		else if( walking ){
+			var nowPos = new Vector2(transform.position.x, transform.position.y);
+			walkingCounter += 0.005f;
+			rigidbody2D.MovePosition( nowPos - walkPos*0.005f);
+
+			if(walkingCounter >= 1){
+				walkingCounter = 0;
+				walkPos = Vector2.zero;
+				walking = false;
+			}
+		}
+	}
+
+	void SetWalk(){
+		var id = Random.Range(1, 10);
+		var obj = GameObject.Find("Appear_"+id);
+
+		var pos = obj.transform.position;
+		walkPos =  new Vector2( transform.position.x-pos.x+Random.Range(-20, 20), transform.position.y-pos.y+Random.Range(-20, 20) );
+		Debug.Log ("Walk : "+id);
 	}
 }
