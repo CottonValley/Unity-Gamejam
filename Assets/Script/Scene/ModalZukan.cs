1c38483589b3901d0200dfb1b1d5a95134c6fbaf
shota_sakumoto <s-sakumoto@flash-edge.com>
2014-11-16 18:27:39 +0900

    cs img add

---
+++ Assets/Script/Scene/ModalZukan.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModalZukan : MonoBehaviour {
+	public GameObject PrefabX;
+
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	void Start ()
+	{
+		Instantiate(this.PrefabX, new Vector3(0, 0, 0), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Script/Scene/ZukanClick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZukanClick : MonoBehaviour {
+
+	void OnGUI(){
+		// プレハブを取得
+		//GUI.Label(new Rect(10,10,100,100), "MenuWindow");
+		var prefab = Resources.Load("Prefabs/ModalZukan");
+		// プレハブからインスタンスを生成
+		var obj = Instantiate(prefab,new Vector3(0, 0, 0), Quaternion.identity);
+		obj.name = prefab.name;
+	}
+
+}
